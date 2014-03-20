2d2df3665809637bb8a15cd27be0db9e1ab59c28
skipidar <anenada@gmail.com>
2014-03-20 21:44:48 +0100

    Initial commit

---
+++ de.proto.search.product.application/src/de/proto/search/product/application/parts/TextPart.java
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * Copyright (c) 2010 - 2013 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *     Lars Vogel <lars.Vogel@gmail.com> - Bug 419770
+ *******************************************************************************/
+package de.proto.search.product.application.parts;
+
+import javax.annotation.PostConstruct;
+
+import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.e4.ui.di.Persist;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.nebula.widgets.nattable.NatTable;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Text;
+
+public class TextPart {
+	
+//	org.apache.commons.logging.LogFactory factory;
+//	org.eclipse.nebula.widgets.nattable.NatTable natTable;
+//	
+	private Text txtInput;
+	private TableViewer tableViewer;
+//
+//	@Inject
+//	private MDirtyable dirty;
+
+	@PostConstruct
+	public void createComposite(Composite parent) {
+		new NatTable(parent);
+	}
+
+	@Focus
+	public void setFocus() {
+//		tableViewer.getTable().setFocus();
+	}
+
+	@Persist
+	public void save() {
+//		dirty.setDirty(false);
+	}
+}
